4c7ebe2f2edc5277e60570baa0f96a01b31d9d73
cosmosanalytics <100080151+cosmosanalytics@users.noreply.github.com>
2025-01-19 10:47:38 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import cv2
+import av
+from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
+from gaze_tracking import GazeTracking
+
+class VideoTransformer(VideoTransformerBase):
+    def __init__(self):
+        self.gaze = GazeTracking()
+
+    def transform(self, frame):
+        img = frame.to_ndarray(format="bgr24")
+        
+        self.gaze.refresh(img)
+        new_frame = self.gaze.annotated_frame()
+        text = ""
+
+        if self.gaze.is_right():
+            text = "Looking right"
+        elif self.gaze.is_left():
+            text = "Looking left"
+        elif self.gaze.is_center():
+            text = "Looking center"
+
+        cv2.putText(new_frame, text, (60, 60), cv2.FONT_HERSHEY_DUPLEX, 2, (255, 0, 0), 2)
+        
+        return av.VideoFrame.from_ndarray(new_frame, format="bgr24")
+
+def main():
+    st.title("Gaze Tracking App")
+
+    webrtc_streamer(
+        key="gaze_tracking",
+        video_transformer_factory=VideoTransformer,
+        async_transform=True,
+    )
+
+if __name__ == "__main__":
+    main()
